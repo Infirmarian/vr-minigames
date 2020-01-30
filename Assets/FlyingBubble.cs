c08297472ca52f9139c261f0a298f288a9f8fcdb
Jason <jasonhu@Jasons-MacBook-Pro.local>
2020-01-29 20:01:55 -0800

    This is what I did for today.

---
+++ Assets/FlyingBubble.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyingBubble : MonoBehaviour {
+    public float maxNumber = 0.1;
+    public float minNumber = 0;
+    public float randomNumber = Random.Range(0, 0.1);
+    [SerializeField]
+    private float verticalSpeed = randomNumber;
+    private Vector3 tempPosition;
+    void Start () 
+    {
+        tempPosition = transform.position;
+    }
+     void FixedUpdate () 
+    {
+        tempPosition.y += verticalSpeed;
+        transform.position = tempPosition;
+    }
+}
