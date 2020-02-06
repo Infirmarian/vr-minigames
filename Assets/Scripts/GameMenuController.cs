05b3bf096e5d93489abe3ae69279b7b8aef9cdc2
Robert Geil <robert@rgeil.net>
2020-02-05 18:53:53 -0800

    Started working on a menu scene

--- Assets/Scripts/GameMenuController.cs
+++ Assets/Scripts/GameMenuController.cs
@@ -2,10 +2,18 @@ using UnityEngine;
 
 public class GameMenuController : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject splashMenu;
     public enum Games
     {
         SOCCER_GOALIE,
         NOSE_POP
     }
-
+    public void StartMinigame(String title)
+    {
+        Debug.Log("Starting minigame "+title);
+    }
+    public void ShowSelectMenu(){
+        splashMenu.SetActive(false);
+    }
 }
