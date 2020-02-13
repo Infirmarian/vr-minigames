fbebf2ce0180f8a9ed952da120dec1088844e8d9
Jason <jasonhu@Jasons-MacBook-Pro.local>
2020-02-12 19:16:36 -0800

    u suck

--- Assets/FlyingBubble.cs
+++ Assets/FlyingBubble.cs
@@ -3,19 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class FlyingBubble : MonoBehaviour {
-    public float maxNumber = 0.1;
-    public float minNumber = 0;
-    public float randomNumber = Random.Range(0, 0.1);
-    [SerializeField]
-    private float verticalSpeed = randomNumber;
-    private Vector3 tempPosition;
+    public float maxNumber = 0.1f;
+    public float minNumber = 0f;
+    public float randomNumber;
+    public float randomNumber2;
+    public float verticalSpeed;
+    public float horizontalSpeed;
+    public Vector3 tempPosition;
     void Start () 
     {
+        randomNumber = Random.Range(0.05f, 0.15f);
+        verticalSpeed = randomNumber;
         tempPosition = transform.position;
     }
      void FixedUpdate () 
     {
         tempPosition.y += verticalSpeed;
+        randomNumber2 = Random.Range(-0.1f, 0.1f);
+        horizontalSpeed = randomNumber2;
+        tempPosition.x += horizontalSpeed;
         transform.position = tempPosition;
     }
 }
