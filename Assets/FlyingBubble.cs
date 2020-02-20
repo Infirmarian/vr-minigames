a758d9de7563bd9e47627211cce0d2ff877de837
Jason <jason431@ucla.edu>
2020-02-19 19:21:04 -0800

    added random speed to bubble & direction of spin

--- Assets/FlyingBubble.cs
+++ Assets/FlyingBubble.cs
@@ -7,25 +7,34 @@ public class FlyingBubble : MonoBehaviour
     public Rigidbody rb;
 
     private float time = 0.0f;
-    private bool isMoving = false;
-    private bool isJumpPressed = false;
     public float speed;
-    public int i = 1;
+    public int randomNumber1;
+    public int randomNumber2;
+    public int randomNumber3;
+    public int binaryInt;
 
     void Start () 
     {
         rb = GetComponent<Rigidbody>();
-        //Set the speed of the GameObject
         speed = 10.0f;
+
+        randomNumber1 = Random.Range(5, 10);
+        randomNumber2 = Random.Range(10, 20);
+        randomNumber3 = Random.Range(1, 2);
+
+        if (randomNumber3 % 2 == 0)
+            binaryInt = 1;
+        else
+            binaryInt = -1;
         
     }
 
     
      void Update () 
     {
-        rb.velocity = new Vector3(0, 2, 0);
+        rb.velocity = new Vector3(0, randomNumber1, 0);
         rb.AddForce(transform.forward * 100);
-        transform.Rotate(new Vector3(0, 10, 0) * Time.deltaTime * speed, Space.World);
+        transform.Rotate(new Vector3(0, randomNumber2 * binaryInt, 0) * Time.deltaTime * speed, Space.World);
         
     }
 }
