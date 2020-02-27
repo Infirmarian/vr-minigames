b859edf6e389d601c53f3363374b2338c5b0c4e9
Kent Zhang <kent@Kents-MacBook-Pro.local>
2020-02-26 19:16:25 -0800

    Prelim Scene for Bubble Game

---
+++ Assets/Scenes/Sphere_Poker/Stabber_Script.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stabber_Script : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision collision){
+        GameObject bubble = collision.gameObject;
+        if(bubble.name == "Bubble(Clone)"){
+            // Animator a = bubble.GetComponent<Animator>();
+            // bubble.SetTrigger("Pop");
+            Destroy(bubble);
+        }
+        else if(bubble.name == "Bubble"){
+            bubble.transform.Translate(new Vector3(0,15,0));
+        }
+    }
+}
